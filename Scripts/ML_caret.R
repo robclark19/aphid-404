e0a01aee0644553948ca77f70033fee7d167a731
Megan Blance <megblance2@gmail.com>
2022-04-06 17:43:12 -0700

    ridge/xgbm now working

--- Scripts/ML_caret.R
+++ Scripts/ML_caret.R
@@ -5,29 +5,32 @@ library(randomForest)
 library(lubridate)
 library(xgboost)
 library(elasticnet)
+library(glmnet)
 
 vegData <- read.csv("./Data/transect_vegetation_data_2022-03-17_cleaned.csv")
 bycatchData <- read.csv("./Data/insect_bycatch_2022-03-17_cleaned.csv", fileEncoding="UTF-8-BOM")
 full_model_data <- read.csv("./Data/insect_veg_combined_2022-03-17.csv")
 
-#dummy variable is used to create one-hot encoding AKA categorical column as a feature converted to numeric 0 or 1
-
-##### Megan Basic ML Algs ---------------------------------------------------
-#Listed in order of most to least transects with occurrence
-#Need to change the call in 3 places: bug_data, ml_formula, and the name in md_split, and sometimes confusionMatrix name
 insects_interest <- c("ciccadellidae", "miridae", "melyridae",
                       'delphacidae', 'aphididae', 'nabidae', 'coccinellidae')
 
+#dummy variable is used to create one-hot encoding AKA categorical column as a feature converted to numeric 0 or 1
+
+##### Megan Basic ML Algs
+#Listed in order of most to least transects with occurrence
+#Need to change the call in 3 places: bug_data, ml_formula, and the name in md_split, and sometimes confusionMatrix name
+
+
 ###
-###  1  modeling with all veg, abundance based
+###  1  modeling with all veg, abundance based ---------------------------------------------
 ###
 #Create the model data with the veg and insect, can't keep all the bugs in until we look at specific response variables
 ##Modifying data as needed
-bug_data <- bycatchData %>%
+rbug_data <- bycatchData %>%
   dplyr::select('site', 'transect', 'sampling_period', insects_interest[2])
-model_data <- vegData %>%
-  dplyr::full_join(bug_data, by = c("sampling_period", "site","transect")) %>%
-  plyr::mutate(
+rmodel_data <- vegData %>%
+  dplyr::full_join(rbug_data, by = c("sampling_period", "site","transect")) %>%
+  dplyr::mutate(
     sampling_period = as.factor(.$sampling_period),
     date = mdy(.$date))
 
@@ -37,115 +40,110 @@ ml_formula <- as.formula(paste0(insects_interest[2],
                                 "~", "."))
 #Data split
 #Need to split data AND do folding. The folding tests on all data so without it the model performs better due to viewing all datapoints
-md_split <- createDataPartition(model_data$miridae, p = 0.7, list = FALSE)
-md_train <- model_data[md_split, !(names(model_data) %in% c('date', 'site', 'transect', 'sampling_period'))]
-md_test <- model_data[-md_split, !(names(model_data) %in% c('date', 'site', 'transect', 'sampling_period'))]
+rmd_split <- createDataPartition(rmodel_data$miridae, p = 0.7, list = FALSE)
+rmd_train <- rmodel_data[rmd_split, !(names(rmodel_data) %in% c('date', 'site', 'transect', 'sampling_period'))]
+rmd_train_sub <- rmd_train %>% dplyr::select(which(!colSums(rmd_train, na.rm=TRUE) %in% 0)) 
+rmd_test <- rmodel_data[-rmd_split, !(names(rmodel_data) %in% c('date', 'site', 'transect', 'sampling_period'))]
+
+
 
 train_control <- trainControl(method = "cv", number = 10, p = 0.7)
 
 #### RF Model
 set.seed(7)
 rf_rmod <- train(form = ml_formula,
-                  data = md_train,
+                  data = rmd_train,
                   method="rf",
                   trControl=train_control)
 rf_rfit <- predict(rf_rmod)
-predict_test <- predict(rf_rmod, md_test)
-plot(md_train$miridae, rf_rfit)
+plot(rmd_train$miridae, rf_rfit)
 
-### Ridge Regression
-#This won't work, missing RMSE values error
-#Glmnet with alpha = 0 is ridge and 1 is lasso
+### Ridge Regression: 2 options
 set.seed(7)
 rid_rmod <- train(form = ml_formula,
-                data = md_train,
+                data = rmd_train_sub,
                 method="ridge",
                 trControl=train_control)
-set.seed(7)
-ridge_rmod <- train(form = ml_formula, data = md_train, method = 'glmnet', tuneGrid = expand.grid(alpha = 0, lambda = 1))
-ridge_fit <- predict(ridge_rmod)
-ridge_rmse <- RMSE(ridge_fit, md_train$miridae)
+rid_rfit <- predict(rid_rmod)
+plot(rmd_train$miridae, rf_rfit)
+
+#Glmnet with alpha = 0 is ridge and 1 is lasso
+# set.seed(7)
+# ridge_rmod <- train(form = ml_formula, data = rmd_train, method = 'glmnet', 
+#                     tuneGrid = expand.grid(alpha = 0, lambda = 1))
+#to find the best lambda later rather than 1 as full penalty: (0, 1, 0.1)
+#ridge_rmse <- RMSE(ridge_fit, md_train$miridae)
 
 ####Gradient Boosted Model
-#This won't work, missing RMSE values error
 
 set.seed(7)
-boo_rmod <- train(form = ml_formula, data=md_train, method = 'xgbTree', trControl = train_control, 
-                  nrounds = 20)
-
-##Boosted model via xgboost package. Need to fix using caret
-xtrain = as.matrix(md_train[,-64])
-ytrain = as.matrix(md_train[,64])
-set.seed(7)
-xgb_train <- xgb.DMatrix(data = xtrain, label = ytrain)
-boo_rmod <- xgboost(data = xgb_train, nrounds = 50)
+boo_rmod <- train(form = ml_formula, data = rmd_train_sub, method = 'xgbTree', trControl = train_control)
+boo_rfit <- predict(boo_rmod)
+plot(rmd_train$miridae, boo_rfit)
 
 #Compare models
 #model_compare <- resamples(RF = rf_rmod, Boosted = boo_rmod, Ridge = rr_fit)
 #summary(models_compare)
 
+
 ###
-###  2  modeling with all veg, pres/abs based ---> all veg p/a or just bug? just the bug right now
-###
+### 2  modeling with all veg, pres/abs based ---------------------------------------------
+### all veg p/a or just bug? just the bug right now
 
 #Create the model data with the veg and insect, can't keep all the bugs in until we look at specific response variables
 ##Modifying data as needed
 ##Need to make the presence/absence a factor to use classification
-bug_data <- bycatchData %>%
+cbug_data <- bycatchData %>%
   dplyr::select('site', 'transect', 'sampling_period', insects_interest[2]) %>%
-  plyr::mutate(sampling_period = as.factor(.$sampling_period),
-         transect = as.factor(.$transect)) %>%
   dplyr::mutate_if(is.numeric, ~1 * (. != 0))
 
-model_data <- vegData %>%
- plyr::mutate(sampling_period = as.factor(.$sampling_period),
-         transect = as.factor(.$transect)) %>%
-  dplyr::full_join(bug_data, by = c("sampling_period", "site","transect")) %>%
-  plyr::mutate(
-    date = mdy(.$date)) %>%
-  dplyr::mutate_if(is.numeric, as.factor)
+cmodel_data <- vegData %>%
+  dplyr::full_join(cbug_data, by = c("sampling_period", "site","transect")) %>%
+  dplyr::mutate(
+    date = mdy(.$date))
 
 #Formula uses all other columns as predictors
 ml_formula <- as.formula(paste0(insects_interest[2],
                                 "~", "."))
 #Data split
-md_split <- createDataPartition(model_data$miridae, p = 0.7, list = FALSE)
-md_train <- model_data[md_split,!(names(model_data) %in% c('date', 'site', 'transect', 'sampling_period'))]
-md_test <- model_data[-md_split,!(names(model_data) %in% c('date', 'site', 'transect', 'sampling_period'))]
+cmd_split <- createDataPartition(cmodel_data$miridae, p = 0.7, list = FALSE)
+cmd_train <- cmodel_data[cmd_split,!(names(cmodel_data) %in% c('date', 'site', 'transect', 'sampling_period'))]
+cmd_train_sub <- cmd_train %>% dplyr::select(which(!colSums(cmd_train, na.rm=TRUE) %in% 0)) 
+cmd_test <- cmodel_data[-cmd_split,!(names(cmodel_data) %in% c('date', 'site', 'transect', 'sampling_period'))]
 
 train_control <- trainControl(method = "cv", number = 10, p = 0.7)
 
 #####Random Forest model
 set.seed(7)
 rf_cmod <- train(form = ml_formula,
-                 data = md_train,
+                 data = cmd_train,
                  method="rf",
                  trControl=train_control)
 rf_cmod
+rf_cfit <- predict(rf_cmod)
+plot(cmd_train$miridae, rf_cfit)
 
 
 ##### Ridge regression
 set.seed(7)
-ridge_cmod <- train(form = ml_formula, data = md_train, method = 'glmnet', tuneGrid = expand.grid(alpha = 0, lambda = 1))
-ridge_cfit <- predict(ridge_cmod)
-ridge_rmse <- RMSE(ridge_cfit, md_train$miridae)
+rid_cmod <- train(form = ml_formula, data = cmd_train, method = 'glmnet', tuneGrid = expand.grid(alpha = 0, lambda = 1))
+rid_cfit <- predict(rid_cmod)
+plot(cmd_train$miridae, rid_cfit)
 
 ####Gradient Boosted Model
-#I think this runs but my computer doesn't like it so need to try at office
-xtrain = as.numeric(as.matrix(md_train[,-64]))
-ytrain = as.factor(as.matrix(md_train[,64]))
 
 set.seed(7)
-boo_rmod <- train(form = ml_formula, data = md_train, method = 'xgbTree', 
-                  trControl = train_control, nrounds = 20)
+boo_cmod <- train(form = ml_formula, data = cmd_train_sub, method = 'xgbTree', 
+                  trControl = train_control)
+boo_cfit <- predict(boo_cmod)
+plot(cmd_train$miridae, boo_cfit)
 
 
 
 
-
-
-predict_test <- predict(rf_cmod, md_test)
-confusionMatrix(reference = md_test$miridae, data = predict_test, mode = 'everything')
+##Confusion matrix for test data
+predict_test <- predict(rf_cmod, cmd_test)
+confusionMatrix(reference = cmd_test$miridae, data = predict_test, mode = 'everything')
 
 
 ##Consider this later
